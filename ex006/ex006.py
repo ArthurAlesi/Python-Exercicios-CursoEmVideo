3d968865b8c82acd967a2372ae8f43687b36eb7b
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-06-07 18:40:12 -0300

    add ex6 e 7

--- ex006/ex006.py
+++ ex006/ex006.py
@@ -1,1 +1,7 @@
-print("ex 00 6")
+num = int(input("Diga um número"))
+dobro = num * 2
+triplo = num * 3
+raizQuadrada = num ** (1/2)
+print("O dobro de {} é {}".format(num,dobro))
+print("O triplo de {} é {}".format(num, triplo))
+print("A raiz de {} é {}".format(num, raizQuadrada))
--- ex007/ex007.py
+++ ex007/ex007.py
@@ -1,1 +1,6 @@
 print("ex007")
+nota1 = int(input("Diga a 1ª nota"))
+nota2 = int(input("Diga a 2ª nota"))
+soma = nota1 + nota2
+media = soma / 2
+print("A media do aluno é %.1f "% media)
