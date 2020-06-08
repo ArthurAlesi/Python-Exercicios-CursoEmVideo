0929fb87e8e636c0a959ffa3d848965c81c40d43
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-06-08 02:22:25 -0300

    exercicio 11

---
+++ ex011/exercicio011.py
@@ -0,0 +1,8 @@
+largura = int(input("Informe a largura da parede em metros"))
+altura = int(input("Informe a altura da parede em metros"))
+area = largura * altura
+tintaNecessaria = area / 2
+
+print('A área da parede é %.2f'% area)
+print('A quantidade necessária de tinta é igual a %.2f litros'% tintaNecessaria)
+
