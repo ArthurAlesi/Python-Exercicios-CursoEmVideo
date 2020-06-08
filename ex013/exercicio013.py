ad567400551d3e6d9603629cbd299f62589ab7b1
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-06-08 02:40:13 -0300

    add exericio 13

---
+++ ex013/exercicio013.py
@@ -0,0 +1,6 @@
+#Faça um exercicio que leia o salário de um funcionário
+# e mostre o seu novo salario com 15% de aumento
+salario = float(input("Diga o seu salario"))
+aumento = 1.15
+novoSalario = salario * aumento
+print("O seu novo salario é R$%.2f"% novoSalario)
