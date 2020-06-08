c72199a002ede0805b93810f984e213ba0c71f20
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-06-08 02:47:22 -0300

    exericio 14

---
+++ ex014/exercicio014.py
@@ -0,0 +1,6 @@
+# Escreva um programa que converta uma
+# temperatura em ºC para ºF
+
+temperaturaEmCelsius = float(input("informe uma tmeperatura em ºC"))
+faren = (9 * temperaturaEmCelsius) /5 + 32
+print("%.2fºC = %.2fºF"% (temperaturaEmCelsius, faren))
