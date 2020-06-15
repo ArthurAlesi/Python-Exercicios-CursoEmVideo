88b4d476aedc9bd274f60a375b58034867112c80
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-06-15 20:22:52 -0300

    add ex 035

--- ex035/exercicio035.py
+++ ex035/exercicio035.py
@@ -1,2 +1,11 @@
 # Desenvolva um programa que leia o comprimento de 3 reretas e diga se
 # elas podem ou nao formar um triângulo
+
+l1 = float(input("Diga o primeiro lado"))
+l2 = float(input("Diga o segundo lado"))
+l3 = float(input("Diga o terceiro lado"))
+if (l1 + l2 < l3) or (l1 + l3 < l2) or (l1 > l2 + l3):
+
+    print("Não é triângulo")
+else:
+    print("É triângulo")
