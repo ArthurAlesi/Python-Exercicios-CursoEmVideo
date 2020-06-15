1d83de35511b6f9e1233df248137dcc859881a44
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-06-15 19:37:47 -0300

    add ex 032

--- ex032/exercicio032.py
+++ ex032/exercicio032.py
@@ -1,2 +1,9 @@
 # Faça um programa que leia um ano qualquer
 # e mostre se ele é bissexo
+
+ano = int(input("diga o ano"))
+
+if (ano % 4 ==0) and (ano%100!=0):
+    print(ano,  " é bissexto")
+else:
+    print(ano, " não Bissexto")
