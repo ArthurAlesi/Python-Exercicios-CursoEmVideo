b79bbd80f6260974fd432cc6d4cbfbf186096e4f
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-06-15 20:05:51 -0300

    add ex54

--- ex033/exercicio033.py
+++ ex033/exercicio033.py
@@ -1,3 +1,23 @@
 # Faça um programa que leia 3 numeros
 #  e mostre qual o maior e qual o menor
 
+num1 = int(input("Diga um numero"))
+num2 = int(input("Diga um numero"))
+num3 = int(input("Diga um numero"))
+
+menor = maior = num1
+
+# menor _______________________________________________
+if(num2 < num1) and (num2 < num3):
+    menor = num2
+if(num3 < num1) and (num3 <num2):
+    menor = num3
+
+# maior _______________________________________________
+if( num2 > num1) and (num2 > num3):
+    maior = num2
+if (num3 > num1) and (num3 > num2):
+    maior = num3
+
+print("Maior: ", maior)
+print("Menor: ", menor)
--- ex034/exercicio034.py
+++ ex034/exercicio034.py
@@ -1,3 +1,11 @@
 # Pergunte o salário de um funcionario e calcule o valor do seu aumento
 # se salarior maior q 1250, calcule o aumento de 10%
 # para salarios menores, o aumento é de 15%
+
+salario = float(input("Diga é o salário"))
+if salario >= 1250:
+    novo = salario * 1.1
+else:
+    novo = salario * 1.15
+
+print("O salario novo é :", novo)
