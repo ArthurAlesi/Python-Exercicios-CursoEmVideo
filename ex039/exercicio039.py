fa5f06d64de687be28d7bcd8ba43e2f1b58d6346
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-06-17 19:14:07 -0300

    aff ex 39

--- ex039/exercicio039.py
+++ ex039/exercicio039.py
@@ -1,4 +1,15 @@
 # faça um programa q leia o ano de nascimento de um jovem e informe de
 # acordo com sua idade se ele ainda vai se alistar ao serviço miliar,
 # se é a hora de se alistar ou se ja passsou do tempo do alistamento
-# mostre quando tempo ja passou se for o caso
+
+from datetime import date
+hoje = date.today().year
+print(hoje)
+nascimento = int(input("Diga o ano que voce nasceu"))
+idade = hoje - nascimento
+if idade == 18:
+    print("Esta na hora de voce se alistar")
+elif idade < 18:
+    print("Voce ainda vai ter q se alistar")
+else:
+    print("Ja passou da hora de voce se alistar")
