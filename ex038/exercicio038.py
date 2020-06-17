986ffd0b1ff44f87924012c39c1a16ef28526ac4
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-06-17 19:07:32 -0300

    ad ex 38

--- ex038/exercicio038.py
+++ ex038/exercicio038.py
@@ -1,2 +1,13 @@
-# escreva um programa que leio dos numeros e compare-os
-#  qual o maior ou se são normais
+# escreva um programa que leia dos numeros e compare-os
+#  qual o maior ou se são iguais
+n1 = int(input("diga um numero inteiros"))
+n2 = int(input("diga um numero inteiro"))
+maior = None
+if n1 > n2:
+    maior = n1
+elif n2 > n1:
+    maior = n2
+else:
+    maior = "são iguais"
+
+print("O numero maior é: ",maior)
