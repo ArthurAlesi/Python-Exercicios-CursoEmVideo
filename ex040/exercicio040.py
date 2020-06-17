df98fe11e4dc776a67566fad2addc5e2a50bece3
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-06-17 19:16:25 -0300

    add ex 40

--- ex040/exercicio040.py
+++ ex040/exercicio040.py
@@ -1,1 +1,9 @@
 # leia duas notas, calcule a media e dia se foi aprovado ou reprovado
+n1 = float(input("Diga a 1 nota"))
+n2 = float(input("Diga a 2 nota"))
+n3 = float(input("Diga a 3 nota"))
+media = (n1 + n2 + n3) /3
+if(media >= 7):
+    print("Voce passou")
+else:
+    print("Voce reprovou")
