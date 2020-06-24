83aa31219160ac572fcee36a7df11a527c1ffc67
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-06-23 21:44:05 -0300

    ex 53

--- ex053/exercicio053.py
+++ ex053/exercicio053.py
@@ -1,3 +1,10 @@
 '''
 leia uma frase e diga se é um palindromo
 '''
+frase = str(input("Digite"))
+fraseSemEspaco = frase.replace(" ","")
+fraseAoContrario = fraseSemEspaco[::-1]
+if fraseSemEspaco == fraseAoContrario:
+    print("É palindromo")
+else:
+    print("não é plandromo")
