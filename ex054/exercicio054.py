dac03e830ebd4829bdb98a29bbe9df8e165bde15
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-06-23 21:53:12 -0300

    ex 54

--- ex054/exercicio054.py
+++ ex054/exercicio054.py
@@ -1,5 +1,12 @@
 '''
 Crie um programa que leia o ano de nascimento de 7 pessoas. no final mostre
 quantas pessoas ainda nao atingiram a maiooridade quantas ja são maioores
-
 '''
+qtdMaior = 0
+for i in range(7):
+    idade = int(input("Diga a idade"))
+    if idade >= 18:
+        qtdMaior += 1
+qtdMenor = 7 - qtdMaior
+print(qtdMaior," pessoas maiores de idade")
+print(qtdMenor, " pesosas menors de idade")
