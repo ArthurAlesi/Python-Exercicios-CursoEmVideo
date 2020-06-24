1b8d35db94dcb7a11da6243ec7e769d3df2241f7
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-06-24 15:34:38 -0300

    ex 58

--- ex057/exercicio057.py
+++ ex057/exercicio057.py
@@ -2,3 +2,8 @@
 leia o sexo mas só aceite os valores de 'm' ou 'f'
 caso esteja errado peça novamente até ter um valor correto
 '''
+sexo = str(input("Diga o sexo"))
+while sexo not in "fFmM":
+    sexo = str(input("Diga o sexo"))
+
+print("O sexo foi", sexo)
--- ex058/exercicio058.py
+++ ex058/exercicio058.py
@@ -4,3 +4,14 @@ o computador deve escolher um numero entre 0 e 10
 só q agora o jogar vai tentar adivinhar até acertar.
 tem q mostrar quantos palpites foram necessarios
 '''
+from random import randint
+sorteado = randint(1,11)
+entrada = int(input("Diga um numero ente 1 e 10"))
+while sorteado != entrada:
+    print("Voce errou")
+    print("tente de novo")
+    entrada = int(input("Diga um numero ente 1 e 10"))
+print()
+print()
+print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+print("Voce acertou")
