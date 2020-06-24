bd92f74cabec2083310c7f099a2c4cc85b1ae07e
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-06-23 21:30:53 -0300

    ex 52

--- ex052/exercicio052.py
+++ ex052/exercicio052.py
@@ -2,3 +2,16 @@
 leia um numero inteiro e diga se ele é ou não um numero primo
 '''
 
+num = int(input("Diga um numero"))
+ePrimo = True
+for i in range(2,num):
+    if(num % i == 0):
+        ePrimo = False
+        break
+
+if ePrimo:
+    print(num, " é primo")
+else:
+    print("Não é primo")
+
+
