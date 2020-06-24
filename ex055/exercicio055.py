c572830ade4e4a64c13cc6c33ae668fd12b1982f
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-06-23 22:08:05 -0300

    ex 50

--- ex055/exercicio055.py
+++ ex055/exercicio055.py
@@ -1,3 +1,17 @@
 '''
 leia o peso de 5 pessoas e diga qual foi o maior e o menor
 '''
+listaPeso = []
+for i in range(5):
+    listaPeso.append(int(input("Diga o peso da pessoa")))
+
+maior = menor = listaPeso[0]
+for i in range(len(listaPeso)):
+    if listaPeso[i] > maior:
+        maior = listaPeso[i]
+
+    if listaPeso[i] < menor:
+        menor = listaPeso[i]
+
+print(menor)
+print(maior)
