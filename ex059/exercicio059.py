a6d164d6d7910846296f8f77b961530c3bcfe44e
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-06-24 15:58:03 -0300

    ex 059

--- ex059/exercicio059.py
+++ ex059/exercicio059.py
@@ -7,3 +7,49 @@ seu programa  deverá realizar a operação solicidade em casa caso
 [4]novos numeros
 [5]sair do programa
 '''
+n1 = float(input("Diga o primeiro valor"))
+n2 = float(input("Diga o segundo valor"))
+menu = 0
+resultado = None
+while menu != 5:
+    print("[1] somar")
+    print("[2] multiplicar")
+    print("[3] maior")
+    print("[4] novos numeros")
+    print("[5] sair do programa")
+    menu = int(input())
+
+
+    if menu == 1:
+        resultado = n1 + n2
+        print(n1," + ", n2, " = ", resultado)
+
+    elif menu == 2:
+        resultado = n1 * n2
+        print(n1, " x ", n2, " = ", resultado)
+
+    elif menu == 3:
+        if n1> n2:
+            resultado = n1
+        elif n1 == n2:
+            resultado = " são iguais"
+        else:
+            resultado = n2
+        print(" O maior é ", resultado)
+
+    elif menu == 4:
+        print("Diga novos numeros")
+        n1 = float(input("Diga o primeiro valor"))
+        n2 = float(input("Diga o segundo valor"))
+        continue
+
+
+    elif menu == 5:
+        print()
+        print("Voce escolheu sair")
+        print()
+
+    else:
+        print(" entrada inválida")
+        print("tente novamente")
+        print()
--- ex060/exercicio060.py
+++ ex060/exercicio060.py
@@ -1,3 +1,8 @@
 '''
 leia um numero qualquer e mostre sue fatorial
 '''
+
+num = int(input("diga um numero"))
+for i in range(1,num):
+    num *= i
+print(num)
