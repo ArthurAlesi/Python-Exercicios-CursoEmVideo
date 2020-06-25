8b4a1127a807cb2003d5c06061c0672aef4b4a74
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-06-25 18:42:39 -0300

    ex 64

--- ex064/exercicio064.py
+++ ex064/exercicio064.py
@@ -4,3 +4,14 @@ só para quando o uusuario digitar 999
 mostre quantos numeros ele digitou e a soma de todos
 
 '''
+soma = 0
+qtdNum = 0
+num = int(input("Diga um numero"))
+while num != 999:
+    soma += num
+    qtdNum +=1
+    num = int(input("Diga um numero"))
+
+print()
+print("Voce digiou ", qtdNum, " numeros")
+print("A soma de todos eles é ", soma)
