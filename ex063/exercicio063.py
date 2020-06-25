992ff48820132bed874dbfd556eca2ad3641e4b2
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-06-25 18:40:15 -0300

    ex 63

--- ex063/exercicio063.py
+++ ex063/exercicio063.py
@@ -2,3 +2,14 @@
 leia um numero inteiro n
 mostre na tela os n primeiros elementos de uma sequencia fibonaci
 '''
+
+qtd = int(input("Diga quantos termos voce quer "))
+t1 = 0
+t2 = 1
+cont = 3
+while cont <= qtd:
+    t3 = t1 + t2
+    print(t3, end = " ")
+    t1 = t2
+    t2 = t3
+    cont +=1
