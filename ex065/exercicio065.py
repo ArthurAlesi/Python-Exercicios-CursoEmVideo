f41336bdac2a30fef13fdbfa8c6117323aecde4c
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-06-25 18:46:43 -0300

    ex 65

--- ex065/exercicio065.py
+++ ex065/exercicio065.py
@@ -5,3 +5,20 @@ o maior e o menor
 o programa deve perguntar se el quer continuar ou nao a digitar válores
 
 '''
+
+num = int(input("Diga um numero"))
+soma = num
+maior = menor = num
+continuar = str(input("Deseja continuar? [s] sim [n] não"))
+while (continuar == 's'):
+    num = int(input("Diga um numero"))
+    soma += num
+    if num > maior:
+        maior = num
+    if num < menor:
+        menor = num
+    continuar = str(input("Deseja continuar? [s] sim [n] não"))
+
+print("O maior foi ", maior)
+print("O menor foi ", menor)
+print("A soma é ", soma)
