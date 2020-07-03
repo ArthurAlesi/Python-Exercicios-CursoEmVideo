6684893161353d2ab0d79fbefb0ead1054497d6b
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-07-03 17:41:55 -0300

    ex 70

--- ex070/exercicio070.py
+++ ex070/exercicio070.py
@@ -6,3 +6,28 @@ quantos produtos custam mais de 1000
 qual o nome do produto mais barato
 '''
 
+total = totMil = menor = cont = 0
+
+maisBarato = ""
+while True:
+    produto = str(input("Nome do produto: "))
+    preco = float(input("Preco em reais: "))
+    cont += 1
+    total += preco
+    if preco > 1000:
+        totMil += 1
+    if cont == 1 or preco < menor:
+        menor = preco
+        maisBarato = produto
+    else:
+        if preco < menor:
+            menor = preco
+    resp = " "
+    while resp not in "sSnN":
+        resp = str(input("Deseja continuar? [s]sim [n]nao "))
+    if resp in "nN":
+        break
+print()
+print("Total: ", total)
+print(totMil," produtos com mais de 1000 reais")
+print("O mais barato custa ", menor," e é um ", maisBarato)
