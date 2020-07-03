9b027c97118fe178776058c00a5f3bbd0d1cf0e8
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-07-03 17:09:02 -0300

    ad ex 68

--- ex068/exercicio068.py
+++ ex068/exercicio068.py
@@ -3,3 +3,26 @@ o jogador devera jogar par ou impar com a maquina
 só pare se o jjogador perdeu
 mostre o total de vitorias
 '''
+
+from random import randint
+escolha = str(input("[P] Par ou [I]impar"))
+vitoria  = 0
+while True:
+    jogador = int(input("diga um numero"))
+    computador = randint(1,11)
+    total = jogador + computador
+    print()
+    print("Voce escolheu: ", jogador)
+    print("Computador escolheu: ",computador )
+    print()
+    if escolha in "pP" and total % 2 == 0:
+        print("Voce ganhou")
+        vitoria += 1
+    elif escolha in "iI" and total % 2 != 0:
+        print("Voce ganhou")
+        vitoria += 1
+    else:
+        print("Voce perdeu")
+        break
+
+print("Voce teve %s vitoria(s)"% vitoria)
