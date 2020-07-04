fb3133ecd51527b4c90cde78150cd205127ab7d1
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-07-04 18:12:21 -0300

    ex 72

--- ex072/exercicio072.py
+++ ex072/exercicio072.py
@@ -4,3 +4,15 @@ com uma contagem por extenso de 0 a 20
 o programa deverá ler um numero pelo teclado entre 0 e 20
 mostralo por extenso
 '''
+listaExtenso = ("zero", "um", "dois" , "tres","quatro","cinco","seis",
+        "sete","oito","nove","dez","onze","doze","treze","catorze"
+        ,"quinze","dezesseis","dezessete","dezoito",
+        "dezenove","vinte")
+while True:
+    num = int(input("Digite um numero entre 0 e 20 "))
+    if (0 <= num <= 20):
+        break
+    else:
+        print("tente novamente")
+
+print("Voce escolheu ", listaExtenso[num])
