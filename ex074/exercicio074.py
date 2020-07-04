f8437bafee7dd90e1c6cd7d5bcc3c30c077aad7e
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-07-04 18:42:54 -0300

    ex 74

--- ex074/exercicio074.py
+++ ex074/exercicio074.py
@@ -3,3 +3,10 @@ gera 5 numeros aleatorios e os coloque numa tbla
 mostre a listagem de numero
 indique o menor e maior da tupla
 '''
+
+from random import  randint
+num = (randint(1,10),randint(1,10),randint(1,10),randint(1,10),randint(1,10))
+print("todos os numeros sorteados: ",num)
+print("maior: ", max(num))
+print("menor: ",min(num))
+
