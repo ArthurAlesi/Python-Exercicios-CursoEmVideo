a54384a52836da573012258903e1432ff69526cf
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-07-04 18:54:09 -0300

    ex 75

--- ex075/exercicio075.py
+++ ex075/exercicio075.py
@@ -5,3 +5,24 @@ quantas vezes apareceu o valor 9
 qual posição do primeiro valor 3
 quantos foram os numeros pares
 '''
+
+numeros = (int(input("Diga numero")),int(input("Diga numero")),
+           int(input("Diga numero")),int(input("Diga numero")))
+
+qtdPar = 0
+posi3 = None
+for i in numeros:
+    if i % 2 == 0:
+        qtdPar += 1
+
+for i in numeros:
+    if i == 3:
+        posi3 = numeros.index(3) + 1
+        break
+    else:
+        posi3 = "nao tem 3 nessa tupla"
+
+print("quantidade de par ", qtdPar)
+print("Posição do primeiro 3: ", posi3 )
+
+
