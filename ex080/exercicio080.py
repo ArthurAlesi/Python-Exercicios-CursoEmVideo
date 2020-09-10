de8832fc4cb3956ecdfef06f40135a8e32babcef
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-09-10 18:19:41 -0300

    Update exercicio080.py

--- ex080/exercicio080.py
+++ ex080/exercicio080.py
@@ -3,3 +3,19 @@ Crie um programa onde o usuario possa difitar cinco valores numericos e cadastre
 uma lista, já na posicao correta de insercao(sem usar o sort(), no final mostre
 a lista ordenada na tela.
 '''
+lista = []
+for i in range(0,5):
+    n = int(input('digite um valor'))
+    if i ==0 or n > lista[-1]:
+        lista.append(n)
+    else:
+        pos = 0
+        while pos < len(lista):
+            if n <= lista[pos]:
+                lista.insert(pos,n)
+                break
+            pos += 1
+
+
+
+print(lista)
