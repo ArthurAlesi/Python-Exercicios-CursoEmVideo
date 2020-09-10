957f0c66a30567e11faad7d60bbb5dd14d446bd2
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-09-10 18:11:06 -0300

    Update exercicio079.py

--- ex079/exercicio079.py
+++ ex079/exercicio079.py
@@ -1,4 +1,18 @@
 '''
-Crie um programa onde o usuario possa digitar
+Crie um programa onde o usuario possa digitar varios valores
+numericos e cadastre-os em uma lista. Caso o numero ja exista la dentro,
+ ele nao sera adicionado. no final, serao exibidos todos os valores
+  unicos digitados em ordem crescente
 
 '''
+
+numero = list()
+while True:
+    n = int(input("Digite um valor"))
+    if n not in numero:
+        numero.append(n)
+    else:
+        print(" Valor dubplicado")
+    r = str(input("DEseja continuar [s/n]"))
+    if r in "nN":
+        break
