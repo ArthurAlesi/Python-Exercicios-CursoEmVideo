ffedece85928df970951a0583a2c22a81c71e259
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-09-10 17:11:25 -0300

    77 78

--- ex077/exercicio077.py
+++ ex077/exercicio077.py
@@ -2,3 +2,11 @@
 Crie um prograam que tenha uma tupla, com varias palavras (sem acentos).
 Depois disso, voce deve mostrar, para cada palavra, quais sao as suas vogais.
 '''
+listaPalavra = ('agua', 'banana', 'caqui', 'damasco')
+
+for i in listaPalavra:
+    print('vogais de ',i , ": ", end=" ")
+    for l in i:
+        if l in 'aeiouAEIOU':
+            print(l, end=" ")
+    print()
--- ex078/exercicio078.py
+++ ex078/exercicio078.py
@@ -3,3 +3,29 @@ Faça um pprograma que leia 5 valoes numericos e guarde-os em uma lista. No fina
 qual foi o maior e o menor valor difitado e as suas respectrivas posiçoes na lista
 
 '''
+lista = []
+for i in range(5):
+    lista.append(int(input("Diga um valor")))
+
+    if i == 0:
+        maior  = menor = lista[i]
+    else:
+        if lista[i] > maior:
+            maior = lista[i]
+        if lista[i] < menor:
+            menor = lista[i]
+
+print(lista)
+print("maior: ", maior, end=" ")
+for i, v in enumerate(lista):
+    if v == maior:
+        print("na posicao", i + 1, end="")
+
+print()
+
+print("menor: ", menor, end=" ")
+for i, v in enumerate(lista):
+    if v == menor:
+        print("na posicao",i + 1, end="")
+
+
