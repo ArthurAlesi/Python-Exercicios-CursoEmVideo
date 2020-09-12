07d4c7643a2c505382d2b7a6456f088f8ea330a4
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-09-12 09:52:00 -0300

    ex 83

--- ex083/exercicio083.py
+++ ex083/exercicio083.py
@@ -3,3 +3,19 @@ Crie um programa onde o usuario digite uma expressao qualquer que use parenteses
 seu aplicativo deverá analisar se a expressao passada esta com os parenteses abertos
 e fechados na ordem correta
 '''
+expressao = str(input("digite uma expressao"))
+pilha = []
+for simb in expressao:
+    if simb == "(":
+        pilha.append("(")
+    elif simb == ")":
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(")")
+            break
+
+if len(pilha) == 0:
+    print("Sua expressao está valida")
+else:
+    print("Sua expressao está errada")
