0305ebdfaea69e9730a57c76d82a77175b253e50
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-09-12 10:41:38 -0300

    ex 85

--- ex085/exercicio085.py
+++ ex085/exercicio085.py
@@ -3,3 +3,19 @@ Crie um programa onde o usuario possa digitar sete valores numericos e dacastre-
 uma lista unica que mantenha separados os valores pares e impares. No final, mnostre
 os valores pares e imapres em ordem crescente
 '''
+
+num = [[], []]
+valor = 0
+for c in range(1,8):
+    valor = int(input(f"Digite o  {c}valor:"))
+    if valor % 2 == 0:
+        num[0].append(valor)
+    else:
+        num[1].append(valor)
+
+
+
+num[0].sort()
+num[1].sort()
+print(num[0])
+print(num[1])
