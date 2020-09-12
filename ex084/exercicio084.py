0acd47022748f1ce412f1e73775a12d8590ed4ab
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-09-12 10:35:34 -0300

    comit ex 84

--- ex084/exercicio084.py
+++ ex084/exercicio084.py
@@ -5,3 +5,26 @@ faça um programa que leia nome, peso de varias pessoas guardando tudo numa list
 - uma listagem com as pessoas mais leves
 
 '''
+temp = []
+princ = []
+mai = men = 0
+while True:
+    temp.append(str(input("diga o nome")))
+    temp.append(float(input("diga o peso")))
+    if len(princ) == 0:
+        mai = meno = temp[1]
+    else:
+        if temp[1] > mai:
+            mai = temp[1]
+        if temp[1] < men:
+            men = temp[1]
+    princ.append(temp[:])
+    temp.clear()
+    resp = str(input("Quer continuar? [S/N]"))
+    if resp in "nN":
+        break
+
+print(princ)
+print(len(princ))
+print(mai)
+print(men)
