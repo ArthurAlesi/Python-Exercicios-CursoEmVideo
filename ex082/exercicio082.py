aab728def6e79e7b9f8217e4ddb1dd6605bc3a34
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-09-12 09:45:38 -0300

    ex 82

--- ex082/exercicio082.py
+++ ex082/exercicio082.py
@@ -4,3 +4,21 @@ Depois disso crie duas listas e extras que vao conter apenas os
  valores pares e os valores impares digitados respectivamente.
  ao final mostre o conteudo das 3 listas geradas
 '''
+num = []
+pares = []
+impares = []
+while True:
+    num.append(int(input("Digite um numero: ")))
+    continuar = str(input("Deseja continuar [S/N]"))
+    if continuar in 'nN':
+        break
+
+for i, v in enumerate(num):
+    if v% 2 ==0:
+        pares.append(v)
+    else:
+        impares.append(v)
+
+print(num)
+print(pares)
+print(impares)
