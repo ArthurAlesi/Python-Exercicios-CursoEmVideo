762a1b2ee514e179b232258a929e7910a3d71e74
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-09-12 09:28:38 -0300

    ex 81

--- ex081/exercicio081.py
+++ ex081/exercicio081.py
@@ -4,3 +4,19 @@ crie um programa que leia varios numeros e coloqueos em uma lista
 - a lista de valores ordenadas de forma decrescente
 - se o valor 5 foi digitado e esta ou nao na lista
 '''
+
+lista = []
+while True:
+    num = int(input("diga um numero"))
+    continuar = str(input("Quer continuar [S/N]" ))
+    lista.append(num)
+    if continuar in "nN":
+        break
+
+print("lista: ", lista)
+lista.sort(reverse=True)
+print("lista invertida: ", lista)
+if 5 in lista:
+    print("5 esta na lista")
+else:
+    print("5 nao esta na lista")
