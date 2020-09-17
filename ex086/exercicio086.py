f89c5e803557fe5fc48cef6cd271e12862d508ff
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-09-17 19:41:27 -0300

    Update exercicio086.py

--- ex086/exercicio086.py
+++ ex086/exercicio086.py
@@ -3,3 +3,11 @@ Crie um programa que crie uma matriz de dimensao 3x3 e preencha
 com valores lidos pelo teclado
 no final mostre a matriz na tela com a formatacao correta
 '''
+matriz = [[0,0,0],[0,0,0],[0,0,0]]
+for i in range(0,3):
+    for j in range(0,3):
+        matriz[i][j] = int(input("diga um valor"))
+for i in range(0,3):
+    for j in range(0,3):
+        print(f"[{matriz[i][j]}]", end = "")
+    print()
