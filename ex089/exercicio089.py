3abbd1d00f46f207ff5b11dc7d677f38cec7e98d
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-09-21 23:12:09 -0300

    Update exercicio089.py

--- ex089/exercicio089.py
+++ ex089/exercicio089.py
@@ -4,3 +4,22 @@ em uma lista composta. no final, mostre um boletim contendo a media de
 cada um e permita que o usuario possa mostrar as notas de cada aluno
 individualmente
 '''
+
+ficha = list()
+while True:
+    nome = str(input("Nome: "))
+    nota1 = float(input("Nota 1: "))
+    nota2 = float(input("Nota 2: "))
+    media = (nota1 + nota2) / 2
+    ficha.append([nome, [nota1, nota2],media])
+    resp = str(input("Quer continuar? [S/N]"))
+    if resp in "nN":
+        break
+print("-=" * 30)
+print(f"{'No':<4}{'nome':<10}{'MEDIA':>8}")
+print("-" * 26)
+for i, a in enumerate(ficha):
+    print(f"{i:<4}{a[0]:<10}{a[2]:>8.1f}")
+while True:
+    print("-" * 35)
+    opc = int(input)
