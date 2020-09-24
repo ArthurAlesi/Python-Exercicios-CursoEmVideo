ed0f0086ddbc0092df9d16ec2d8fdbabcb480cdd
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2020-09-23 23:09:23 -0300

    ad ex 91

---
+++ ex091/exercicio091.py
@@ -0,0 +1,7 @@
+'''
+Crie um programa onde 4 jogadores joguem um dado e tenham resultados
+aleatorios. guarde esses resultados em um dicionario. no final, coloque
+ esses dicionarios em ordem, sabendio que o cencedor tirou o maior numero do dado
+
+
+'''
